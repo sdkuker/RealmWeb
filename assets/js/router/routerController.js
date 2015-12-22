6235259c6c9faefd6950f542451435711d5279d7
sdkuker <sdkuker@gmail.com>
2015-12-21 21:06:55 -0600

    Added separate requireJS calls for each function on the router instead of all at the beginning.

--- assets/js/router/routerController.js
+++ assets/js/router/routerController.js
@@ -1,42 +1,40 @@
-define(['realmApplication',
-        'views/workInProgressView',
-        'views/dieRoller/dieRollerView',
-        'models/dieRoller/dieModel',
-        'views/combat/combatEncounterList',
-        'views/player/playerListView',
-        'views/player/playerView',
-        'models/player/playerModel',
-        'collections/player/playerCollection',
-        'services/playerServices',
-        'views/player/playerListLayoutView',
-        'views/player/playerListButtonView'
+define(['realmApplication'
     ],
-    function (RealmApplication,  WorkInProgressView, DieRollerView, DieModel, CombatEncounterListView,
-              PlayerListView, PlayerView, PlayerModel, PlayerCollection, PlayerServices, PlayerListLayoutView,
-              PlayerListButtonView) {
+    function (RealmApplication) {
 
         RouterController = {
             workInProgress: function () {
-                var view = new WorkInProgressView();
-                RealmApplication.regions.mainRegion.show(view);
+                require(['views/workInProgressView'], function (WorkInProgressView) {
+                    var view = new WorkInProgressView();
+                    RealmApplication.regions.mainRegion.show(view);
+                });
             },
             dieRoller: function () {
-                var dieModel = new DieModel();
-                var view = new DieRollerView({model: dieModel});
-                RealmApplication.regions.mainRegion.show(view);
+                require(['models/dieRoller/dieModel','views/dieRoller/dieRollerView'], function (DieModel, DieRollerView) {
+                    var dieModel = new DieModel();
+                    var view = new DieRollerView({model: dieModel});
+                    RealmApplication.regions.mainRegion.show(view);
+                });
             },
             combatEncounterList: function () {
-                var view = new CombatEncounterListView();
-                RealmApplication.regions.mainRegion.show(view);
+                require(['views/combat/combatEncounterList',], function (CombatEncounterListView) {
+                    var view = new CombatEncounterListView();
+                    RealmApplication.regions.mainRegion.show(view);
+                });
             },
             playerList: function () {
-                var playerListLayoutView = new PlayerListLayoutView();
-                RealmApplication.regions.mainRegion.show(playerListLayoutView);
-                var playerCollection = PlayerServices.getAllPlayers();
-                var playerListView = new PlayerListView({collection: playerCollection});
-                var playerListButtonView = new PlayerListButtonView();
-                playerListLayoutView.getRegion('playerTableRegion').show(playerListView);
-                playerListLayoutView.getRegion('buttonsRegion').show(playerListButtonView);
+                require(['views/player/playerListView', 'views/player/playerView', 'collections/player/playerCollection',
+                    'services/playerServices','views/player/playerListLayoutView','views/player/playerListButtonView'],
+                    function (PlayerListView, PlayerView, PlayerCollection, PlayerServices, PlayerListLayoutView,
+                              PlayerListButtonView) {
+                    var playerListLayoutView = new PlayerListLayoutView();
+                    RealmApplication.regions.mainRegion.show(playerListLayoutView);
+                    var playerCollection = PlayerServices.getAllPlayers();
+                    var playerListView = new PlayerListView({collection: playerCollection});
+                    var playerListButtonView = new PlayerListButtonView();
+                    playerListLayoutView.getRegion('playerTableRegion').show(playerListView);
+                    playerListLayoutView.getRegion('buttonsRegion').show(playerListButtonView);
+                });
             }
         };
 
