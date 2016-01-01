ccc6fef7afcd72b580fb35b41373f8a91fe8c688
sdkuker <sdkuker@gmail.com>
2016-01-01 12:36:31 -0600

    2nd part of got type stuff to work with severities.

--- assets/js/views/criticalHit/criticalHitFilterView.js
+++ assets/js/views/criticalHit/criticalHitFilterView.js
@@ -22,13 +22,31 @@ define(['marionette',
             //    RealmApplication.vent.trigger('playerListDeleteButton:clicked');
             },
             onRender : function() {
+                var typeSelectElement = this.$el.find('#typeSelect');
                 var allTypes = this.options.criticalHits.getAllTypes();
                 for(type in allTypes) {
-                    this.$el.find('#typeSelect').append("<option value='" + type + "'>" + type + "</option>");
+                    typeSelectElement.append("<option value='" + type + "'>" + type + "</option>");
                 };
-                var allSeverities = this.options.criticalHits.getAllSeverities();
+
+                var selectedType = $('#typeSelect option:selected').val();
+                if (! selectedType) {
+                    var firstSelectOption = typeSelectElement.find('option:first');
+                    if (firstSelectOption) {
+                        firstSelectOption.attr('selected', true);
+                        selectedType = firstSelectOption.val();
+                    }
+                };
+
+                var allSeverities = null;
+                if (selectedType) {
+                    allSeverities = this.options.criticalHits.getSeveritiesForType(selectedType);
+                } else {
+                    allSeverities = this.options.criticalHits.getAllSeverities();
+                }
                 for(severity in allSeverities) {
-                    this.$el.find('#severitySelect').append("<option value='" + severity + "'>" + severity + "</option>");
+                    if (severity && severity != 'undefined') {
+                        this.$el.find('#severitySelect').append("<option value='" + severity + "'>" + severity + "</option>");
+                    };
                 };
 
             },
