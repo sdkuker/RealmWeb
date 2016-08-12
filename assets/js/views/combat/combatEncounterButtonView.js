e6b03d1baabe81a28e92cb4dedb4c186ca524f04
sdkuker <sdkuker@gmail.com>
2016-08-12 18:43:31 -0500

    Got the displayed round select to populate and init properly.  Still need to get views to listed to selection events.

--- assets/js/views/combat/combatEncounterButtonView.js
+++ assets/js/views/combat/combatEncounterButtonView.js
@@ -10,16 +10,35 @@ define(['marionette',
             model: CombatEncounterModel,
             initialize : function(options) {
                 self = this;
+                self.displayedRound = options.model.get('openRound');
             },
+            displayedRound : null,
             events : {
                 'click #nextRoundButton' : 'nextRoundButtonClicked',
                 'click #criticalHitsButton' : 'criticalHitsButtonClicked'
+            },
+            onRender : function() {
+                var self = this;
+                self.populateRounds();
             },
             nextRoundButtonClicked : function() {
                 this.trigger('combatEncounterNextRoundButton:clicked');
             },
             criticalHitsButtonClicked : function() {
                 RealmApplication.vent.trigger('criticalHitsButton:clicked');
+            },
+            populateRounds : function() {
+                var self = this;
+                var displayedRoundsSelectElement = this.$el.find('#displayedRound');
+                displayedRoundsSelectElement.empty();
+                var roundsSelectElement = null;
+                for(index = 1; index <= this.model.get('openRound'); index ++) {
+                    displayedRoundsSelectElement.append("<option value='" + index + "'>" + index + "</option>");
+                };
+               // var openSelectOption = displayedRoundsSelectElement.find('option:first');
+                //displayedRoundsSelectElement.attr('selected', true);
+                $('#displayedRound', this.$el ).val(self.displayedRound);
+
             },
         });
 
