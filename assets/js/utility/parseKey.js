f8f1950e1deb39c7c00850d74999c10b0cf65e24
sdkuker <sdkuker@gmail.com>
2017-01-16 11:26:14 -0600

    Removed the old parse files and keys.

--- assets/js/utility/parseKey.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'parse'], function ($, Parse) {
-        "use strict";
-        Parse.$ = $;
-        Parse.initialize("BITvZDeeYrJSDU14nuVecP4SNj2o2AWs7lM1NKGE", "P3qHbYWcMkBefEghK2brEx3RaKJAz2qDeaRWpGpv");
-
-        return Parse;
-    }
-);
